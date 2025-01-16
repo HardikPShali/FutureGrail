5ca4e6f2da1959295c2f914d66e37200c8719cac
Hardik Shali <hardikshali1612@gmail.com>
2025-01-16 23:46:32 +0100

    code upload

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,3 @@
-import { getJestProjects } from '@nrwl/jest';
-
-export default {
-  projects: getJestProjects(),
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:abbd4870e6cdae995649389509128a701a2e6cb65d895f8dcb8140e72475575d
+size 98
--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,3 @@
-const nxPreset = require('@nrwl/jest/preset').default;
-
-module.exports = { ...nxPreset };
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e808f951f40e5743f4d5f8e3c060dcae41a7f7e55d2987ca37a6e1e25cffad6
+size 90
